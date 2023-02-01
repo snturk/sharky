f5e94252624d377b921d5fd2e453f5e9d4b2f03a
Muratcan Şentürk <muratcan.senturk@ozguryazilim.com.tr>
2023-02-01 19:53:55 +0300

    feat: Object Permission Check
    * Simplifying packages
    
    Refs: #1

--- security/enforcer.go
+++ security/enforcer.go
@@ -5,21 +5,13 @@ import (
 )
 
 type enforcer struct {
-	// TODO: Fields needed. Issue #1
-	lastResult bool
-}
-
-func InitializeEnforcer(params ...string) {
-	// TODO: Enforcer needs parameters to initialize, relevant params will be added. Issue #1
 }
 
 func GetEnforcer() (enforcer, error) {
-	// TODO: Implement GetEnforcer method. Issue #1
 	return enforcer{}, nil
 }
 
 func (e enforcer) Enforce(context model.PermissionDecisionContext) bool {
-	// TODO: Improve basic implementation Issue #1
 	obj := context.GetObject()
 	role := context.GetRole()
 	action := context.GetAction()
--- security/permission_registry.go
+++
@@ -1,3 +0,0 @@
-package security
-
-// TODO: Implement registry. Issue #1
